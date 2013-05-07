aff019407ff83d00349f6b460e0e7143ab920dab
Roy Bellingan <admin@seisho.us>
2013-05-07 14:01:37 +0200

    Short

--- bins.cpp
+++ bins.cpp
@@ -7,8 +7,8 @@
 
 
 bins::bins(QWidget *parent) :    QMainWindow(parent),    ui(new Ui::bins){
-    //QShortcut *shortcut = new QShortcut(QKeySequence("Ctrl+O"),&parent);
-    //QObject::connect(shortcut, SIGNAL(activated()), this, SLOT(on_nome_triggered()));
+    QShortcut *shortcut = new QShortcut(QKeySequence("Ctrl+O"),this);
+    QObject::connect(shortcut, SIGNAL(activated()), this, SLOT(on_nome_triggered()));
 
     ui->setupUi(this);
     ui->add_bins->setShortcut(QKeySequence("Ctrl+1"));
